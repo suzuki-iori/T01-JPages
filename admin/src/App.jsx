6bac5b4862875d465e5425c786d08a097661aa61
iori <suzukiiori1223@gmail.com>
2026-02-11 21:23:59 +0900

    feat:Appファイルから削除

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -6,7 +6,6 @@ import QuestionnairePage from './components/pages/questionnaire/QuestionnairePag
 import ReceptionPage from './components/pages/reception/ReceptionPage';
 import VisitorsLogPage from './components/pages/visitorslog/VisitorsLogPage';
 import StudentPage from './components/pages/student/StudentPage';
-import AreaPage from './components/pages/area/AreaPage';
 import Detail from './components/pages/mainteam/detail/Detail';
 
 import DetailStudent from './components/pages/student/detailStudent/DetailStudent';
@@ -16,23 +15,19 @@ import VisitorsLogDetail from './components/pages/visitorslog/visitorsLogDetail/
 function App() {
 
   return (
-    <>
-        <Routes>
-                  <Route path='/admin/' element={<Dashboard />}/>
-                  <Route path='/admin/login' element={<Login />}/>
-                  <Route path='/admin/team' element={<MainTeamPage />}/>
-                  <Route path='/admin/question' element={<QuestionnairePage />}/>
-                  <Route path='/admin/questionnairedetail/:id' element={<QuestionnaireDetail/>}/>
-                  <Route path='/admin/reception' element={<ReceptionPage />}/>
-                  <Route path='/admin/visitor' element={<VisitorsLogPage />}/>
-                  <Route path='/admin/visitor/:id' element={<VisitorsLogDetail/>}/>
-                  <Route path='/admin/student' element={<StudentPage />}/>
-                  <Route path='/admin/student/:id' element={<DetailStudent />}/>
-                  <Route path='/admin/area' element={<AreaPage />}/>
-                  <Route path='/admin/team/:id' element={<Detail />} />
-        </Routes>
-
-    </>
+    <Routes>
+      <Route path='/admin/' element={<Dashboard />}/>
+      <Route path='/admin/login' element={<Login />}/>
+      <Route path='/admin/team' element={<MainTeamPage />}/>
+      <Route path='/admin/question' element={<QuestionnairePage />}/>
+      <Route path='/admin/questionnairedetail/:id' element={<QuestionnaireDetail/>}/>
+      <Route path='/admin/reception' element={<ReceptionPage />}/>
+      <Route path='/admin/visitor' element={<VisitorsLogPage />}/>
+      <Route path='/admin/visitor/:id' element={<VisitorsLogDetail/>}/>
+      <Route path='/admin/student' element={<StudentPage />}/>
+      <Route path='/admin/student/:id' element={<DetailStudent />}/>
+      <Route path='/admin/team/:id' element={<Detail />} />
+    </Routes>
   )
 }
 
