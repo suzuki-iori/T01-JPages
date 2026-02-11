cb53731e3d35a5e2cac51b6dc2450d58c46fb3fa
iori <suzukiiori1223@gmail.com>
2026-02-12 05:59:39 +0900

    feat:学生にリンクを追加

--- admin/src/components/common/teamdetail/TeamDetail.jsx
+++ admin/src/components/common/teamdetail/TeamDetail.jsx
@@ -1,4 +1,5 @@
 import React, { useState , useEffect} from 'react';
+import { Link } from 'react-router-dom';
 import styles from './TeamDetail.module.css'
 import { useParams } from 'react-router-dom';
 import { useAuth } from '../../../context/AuthContext';
@@ -96,7 +97,7 @@ export default function TeamDetail(props) {
                                 <span>メンバー</span>
                                 <div className={styles.memberList}>
                                     {teamDetail.team.students.map((student, index) => (
-                                        <p key={index}>{student.grade + "年  " + student.name}</p>
+                                        <p key={index}><Link to={`/admin/student/${student.id}`}>{student.grade + "年  " + student.name}</Link></p>
                                     ))}
                                 </div>
                             </div>
